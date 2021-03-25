366ab486b4629716ffcfe7dac3c1722acc397eea
Bruno-DC <bruno.duarte22@outlook.com>
2021-03-25 13:55:19 -0300

    Adicionando primeiros projetos

---
+++ Conta_Bancaria/DIO.Bank/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DIO.Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarContas();
+                        break;
+                    case "2":
+                        InserirConta();
+                        break;
+                    case "3":
+                        Transferir();
+                        break;
+                    case "4":
+                        Sacar();
+                        break;
+                    case "5":
+                        Depositar();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+            Console.WriteLine("Obrigado por utilizar nossos serviços.");
+            Console.ReadLine();            
+        }
+    }
+}
---
+++ instagram-dio/README.md
@@ -0,0 +1,7 @@
+# Projeto Recriando a página inicial do Instagram
+
+## Bootcamp Html Web Developer
+
+Essas são as alterações realizadas com base no projeto original do curso. 
+
+
